da6d9568c58f70b3c6503e55de98b9b3033be013
Pawel <pawel.konopka5@gmail.com>
2022-07-27 13:47:33 +0200

    Tasks 1 & 2

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,18 @@
+import pytest
+
 from function import square
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+
+
+class Tests:
+    def test_1(self):
+        assert square(0) == 0
+
+    def test_2(self):
+        assert square(1) == 1
+
+    def test_3(self):
+        assert square(-1) == 1
+
+    def test_4(self):
+        assert square(100) == 10_000
--- Pytest/Task2/function.py
+++ Pytest/Task2/function.py
@@ -1,7 +1,19 @@
+# def is_prime(x):
+#     if x == 0 or x == 1:
+#         return False
+#     for i in range(2, x):
+#         if not x % i:
+#             return False
+#     return True
+
+
 def is_prime(x):
-    if x == 0 or x == 1:
+    if x == 2 or x == 3:
+        return True
+    if x < 2 or x % 2 == 0 or x % 3 == 0:
         return False
-    for i in range(2, x):
-        if not x % i:
+    
+    for i in range(6, int(x ** 0.5) + 2, 6):
+        if x % (i-1) == 0 or x % (i+1):
             return False
     return True
